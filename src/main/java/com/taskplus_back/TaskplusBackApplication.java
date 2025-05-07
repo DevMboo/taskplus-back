66afa4b2d8d2e1782e2dfb078ce9cf6eb9d4bfb1
DevMboo <lchavesdesousa@gmail.com>
2025-05-06 23:34:32 -0300

    initial

---
+++ src/main/java/com/taskplus_back/TaskplusBackApplication.java
@@ -0,0 +1,13 @@
+package com.taskplus_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskplusBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskplus_back/TaskplusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskplus_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskplusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
